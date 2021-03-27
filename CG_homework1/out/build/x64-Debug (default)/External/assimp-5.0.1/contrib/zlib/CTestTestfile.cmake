537901c5d0da3aa90320b159ea584aa9c3fd7c5b
Naomi <41462684+naomixie@users.noreply.github.com>
2021-03-27 18:11:15 +0800

    fix: CleanUp master

--- CG_homework1/out/build/x64-Debug (default)/External/assimp-5.0.1/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/naomi/Desktop/CG_homework1/CG_homework1/External/assimp-5.0.1/contrib/zlib
-# Build directory: C:/Users/naomi/Desktop/CG_homework1/CG_homework1/out/build/x64-Debug (default)/External/assimp-5.0.1/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
